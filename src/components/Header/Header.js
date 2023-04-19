7ff37a359d8f2bf7206e6299cdba9c6e8983d6cb
avazbekowna <almamatova.erkaiym03@gmail.com>
2023-04-19 12:31:37 +0600

    header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div id="header">
+            <div className="container">
+                <div className="header">
+                    <h1>Aisedep</h1>
+                    <p>Erkaiym</p>
+
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Header;
